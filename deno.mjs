326528608c362fcbfec0c269923f4e47121904d3
Muhammad Suliman <sulmanfarooq27@gmail.com>
2026-09-02 00:00:33 +0500

    feat: enable resilient key rotation for Deno server

--- deno.mjs
+++ deno.mjs
@@ -1,7 +1,7 @@
 //deprecated:
 //import {serve} from "https://deno.land/std/http/mod.ts"
 
-import worker from "./src/worker.mjs";
+import worker from "./src/robust-worker.mjs";
 
 const port = +(Deno.env.get("PORT") ?? 8080);
 
